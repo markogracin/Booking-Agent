d5d1d28a863ff76c0dca22389b34b37769520e3d
Marko Gracin <marko.gracin@gmail.com>
2017-10-23 21:43:41 +0200

    Add files via upload

--- private/reservationInsert.php
+++ private/reservationInsert.php
@@ -1,7 +1,7 @@
 <?php
 include_once "../config.php";
 
-if(empty($_POST['guest_name'] && $_POST['guest_email'] && $_POST['property'] && $_POST['date_from'] && $_POST['date_to'])){
+if(empty($_POST['guest_name'] && $_POST['guest_email'] && $_POST['property_id'] && $_POST['date_from'] && $_POST['date_to'])){
   header("location:reservation.php?oh_gosh!");
 }else{
 
@@ -10,7 +10,7 @@ $do=$db->prepare("insert into reservation (guest_name, guest_email, property_id,
 $do->execute(array(
 	"guest_name" => $_POST['guest_name'], 
 	"guest_email" => $_POST["guest_email"], 
-	"property" => $_POST["property_id"],
+	"property_id" => $_POST["property_id"],
 	"date_from" => $_POST["date_from"],
 	"date_to" => $_POST["date_to"]
 	));
